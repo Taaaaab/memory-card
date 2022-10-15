c099a16f10709be8d560317355d2c510b6feebe5
Taaaaab <owen.whitsett@gmail.com>
2022-10-15 14:16:44 -0400

    Update animal cards and stylesheet for memory game

--- src/components/Game.js
+++ src/components/Game.js
@@ -1,4 +1,10 @@
-import React, {useState} from "react";
+import React, {useState} from 'react';
+import bb from './bb.jpg';
+import mk from './mk.webp';
+import ltm from './ltm.jpeg'
+import sg from './sg.jpeg';
+import pc from './pc.jpg';
+import rtl from './rtl.webp';
 
 const Game = () => {
     const [score, setScore] = useState(0);
@@ -14,12 +20,30 @@ const Game = () => {
                 <button onClick={incrementCount}>Increment</button>
             </div>
             <div className="cards">
-                <div className="card">Bush Baby</div>
-                <div className="card">Meerkat</div>
-                <div className="card">Long-tailed Macaque</div>
-                <div className="card">Sugar Glider</div>
-                <div className="card">Persian Cat</div>
-                <div className="card">Card6</div>
+                <div className="card">
+                    <img src={bb} className="animal" alt="bush baby" />
+                    <p>Bush Baby</p>
+                </div>
+                <div className="card">
+                    <img src={mk} className="animal" alt="meerkat" />
+                    <p>Meerkat</p>
+                </div>
+                <div className="card">
+                    <img src={ltm} className="animal" alt="long tailed macaque" />
+                    <p>Long-tailed Macaque</p>
+                </div>
+                <div className="card">
+                    <img src={sg} className="animal" alt="sugar glider" />
+                    <p>Sugar Glider</p>
+                </div>
+                <div className="card">
+                    <img src={pc} className="animal" alt="persian cat" />
+                    <p>Persian Cat</p>
+                </div>
+                <div className="card">
+                    <img src={rtl} className="animal" alt="ring tailed lemur" />
+                    <p>Ring Tailed Lemur</p>
+                </div>
             </div>
         </div>
     );
