bff94331463e396e90662e139be06b023d5dfd0e
Taaaaab <owen.whitsett@gmail.com>
2022-12-11 13:47:28 -0500

    Setup for deployment

--- src/App.js
+++ src/App.js
@@ -5,7 +5,9 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h3>Animal Guessing Game</h3>
+        <h2>Animal Guessing Game</h2>
+        <div>Get points by clicking on an image but don't click on any more than once!</div>
+        <p></p>
         <Game />
       </header>
     </div>
