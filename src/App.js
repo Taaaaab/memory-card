0a0d5a85c7e0c7228f8941fc431bde0b705d3dd1
Taaaaab <owen.whitsett@gmail.com>
2022-12-11 14:17:20 -0500

    Re-configured grid, cards and updated stylesheet

--- src/App.js
+++ src/App.js
@@ -3,13 +3,13 @@ import Game from './components/Game';
 
 function App() {
   return (
-    <div className="App">
+    <div className="container">
       <header className="App-header">
         <h2>Animal Guessing Game</h2>
         <div>Get points by clicking on an image but don't click on any more than once!</div>
-        <p></p>
-        <Game />
       </header>
+      <Game />
+      <footer>Created by Owen W. 2022</footer>
     </div>
   );
 }
